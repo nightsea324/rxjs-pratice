1637e186e197b0bd024b824940f1b97dc87ad98f
NightSea <sweetmilkshake0324@gmail.com>
2022-08-04 16:33:19 +0800

    feat: create 拖拉方塊

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { concatAll, fromEvent, map, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'rxjs-pratice';
+
+  ngOnInit(): void {
+    const dragDom = document.getElementById('drag');
+    const body = document.body;
+    if (dragDom && body) {
+      const mouseDown = fromEvent<MouseEvent>(dragDom, 'mousedown');
+      const mouseUp = fromEvent<MouseEvent>(body, 'mouseup');
+      const mouseMove = fromEvent<MouseEvent>(body, 'mousemove');
+      mouseDown
+        .pipe(
+          map(() => mouseMove.pipe(takeUntil(mouseUp))),
+          concatAll()
+        )
+        .subscribe((event) => {
+          dragDom.style.left = `${event.clientX}px`;
+          dragDom.style.top = `${event.clientY}px`;
+        });
+    }
+  }
 }
