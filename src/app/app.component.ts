508d23b4a5705c6ce62eea0ca918758b61114597
NightSea <sweetmilkshake0324@gmail.com>
2022-08-05 10:51:17 +0800

    feat: create drag video

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { concatAll, fromEvent, map, takeUntil } from 'rxjs';
+import {
+  concatAll,
+  filter,
+  fromEvent,
+  map,
+  takeUntil,
+  withLatestFrom,
+} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,29 +17,78 @@ export class AppComponent implements OnInit {
   title = 'rxjs';
 
   ngOnInit(): void {
-    this.drag();
+    this.scrollVideo();
+    this.dragVideo();
   }
 
   /**
-   * drag - 拖拉方塊事件
+   * scrollVideo - 滾動影片
    */
-  drag() {
+  scrollVideo() {
     // get element
-    const dragDom = document.getElementById('drag') as HTMLElement;
-    const body = document.body;
+    const video = document.getElementById('video') as HTMLElement;
+    const anchor = document.getElementById('anchor') as HTMLElement;
     // set event
-    const mouseDown = fromEvent<MouseEvent>(dragDom, 'mousedown');
-    const mouseUp = fromEvent<MouseEvent>(body, 'mouseup');
-    const mouseMove = fromEvent<MouseEvent>(body, 'mousemove');
-    // rxjs drag
+    const scroll = fromEvent(document, 'scroll');
+    // rx
+    scroll
+      .pipe(map(() => anchor.getBoundingClientRect().bottom < 0))
+      .subscribe((bool) => {
+        if (bool) {
+          video.classList.add('video-fixed');
+          return;
+        }
+        video.classList.remove('video-fixed');
+        return;
+      });
+  }
+
+  /**
+   * dragVideo - 拖拉影片
+   */
+  dragVideo() {
+    // get element
+    const video = document.getElementById('video') as HTMLElement;
+    // set event
+    const mouseDown = fromEvent<MouseEvent>(video, 'mousedown');
+    const mouseUp = fromEvent<MouseEvent>(document, 'mouseup');
+    const mouseMove = fromEvent<MouseEvent>(document, 'mousemove');
+    // rx
     mouseDown
       .pipe(
+        filter(() => video.classList.contains('video-fixed')),
         map(() => mouseMove.pipe(takeUntil(mouseUp))),
-        concatAll()
+        concatAll(),
+        withLatestFrom(mouseDown, (move, down) => {
+          return {
+            x: this.validValue(
+              move.clientX - down.offsetX,
+              window.innerWidth - 320,
+              0
+            ),
+            y: this.validValue(
+              move.clientY - down.offsetY,
+              window.innerHeight - 180,
+              0
+            ),
+          };
+        })
       )
       .subscribe((event) => {
-        dragDom.style.left = `${event.clientX}px`;
-        dragDom.style.top = `${event.clientY}px`;
+        video.style.top = `${event.y}px`;
+        video.style.left = `${event.x}px`;
       });
+  }
+
+  /**
+   * validValue - 規範最大最小值
+   *
+   * @param value - number
+   * @param max - number
+   * @param min - number
+   * @returns number
+   */
+  validValue(value: number, max: number, min: number): number {
+    return Math.min(Math.max(value, min), max);
   }
 }
